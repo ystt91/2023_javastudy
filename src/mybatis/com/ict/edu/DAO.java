3fd2de63ada643407a0fb617be8bee2117d386bd
Youngsong <poiu62875@gmail.com>
2023-06-12 17:57:36 +0900

    0612

--- src/mybatis/com/ict/edu/DAO.java
+++ src/mybatis/com/ict/edu/DAO.java
@@ -1,5 +1,7 @@
 package mybatis.com.ict.edu;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 
 //DB 처리하는 메서드들을 가지고 있는 클래스
@@ -17,5 +19,45 @@ public class DAO {
 	
 	// DB 처리하는 메서드들
 	
+	// customer 테이블 전체 보기
+	// select, 결과는 여러개, 파라미터가 없음
+	public static List<VO> getList(){
+		List<VO> list = null;
+		
+		//selectList() : 결과가 하나 이상일 때 사용하는 메서드
+		//selectOne() : 결과가 반드시 하나일 때 사용하는 메서드
+		
+		// 파리미터가 있는 메서드와 파라미터가 없는 메서드로 나눈다.
+		// 파라미터가 있는 메서드 : selecList("mapper의 id", 파라미터);
+		// 파라미터가 없는 메서드 : selectList("mapper의 id");
+		list = getSession().selectList("custList");
+		return list;
+	}
+	
+	//select, 결과는 하나, 파라미터 있음(String)
+	public static VO getOne(String custid) {
+		VO vo = getSession().selectOne("custOne", custid);
+		return vo;
+	}
+	
+	// insert, delete, update 결과가 int(숫자), 파라미터는 무조건 있다.
+	// 반드시 ***commit를 해야 된다.
+	public static int getInsert(VO vo) {
+		int result = getSession().insert("custIns", vo);
+		ss.commit();
+		return result;
+	}
+	
+	public static int getDelete(VO vo) {
+		int result = getSession().delete("custDel", vo);
+		ss.commit();
+		return result;
+	}
+	
+	public static int getUpdate(VO vo){
+		int result = getSession().update("custUpdate", vo);
+		ss.commit();
+		return result;
+	}
 	
 }
--- src/mybatis/com/ict/edu/Ex01.java
+++ src/mybatis/com/ict/edu/Ex01.java
@@ -1,5 +1,6 @@
 package mybatis.com.ict.edu;
 
+import java.util.List;
 import java.util.Scanner;
 
 // 마이바티스 세팅하기
@@ -15,6 +16,9 @@ import java.util.Scanner;
 public class Ex01 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		
+		Ex01 test = new Ex01();
+		
 		System.out.println("선택하세요");
 		System.out.println("1. customer 테이블 전체보기");
 		System.out.println("2. customer 테이블 특정 데이터 보기");
@@ -24,15 +28,96 @@ public class Ex01 {
 		System.out.print(">>>>>>>>>>  ");
 		
 		int menu = scan.nextInt();
-		
+		List<VO> list = null;
 		switch (menu) {
-		case 1:  break;
-		case 2:  break;
-		case 3:  break;
-		case 4:  break;
-		case 5:  break;
+			case 1: list = DAO.getList(); test.prn(list); break;
+			case 2:  
+				System.out.println("custid를 입력하세요 >>>") ;
+				String custid = scan.next();
+				VO vo = DAO.getOne(custid);
+				test.prn2(vo);				
+				break;
+				
+			case 3:  
+				System.out.print("삽입정보를 입력하세요");
+				System.out.print("이름 : ");
+				String name = scan.next();
+				System.out.print("주소 : ");
+				String address = scan.next();
+				System.out.print("전화번호 : ");
+				String phone = scan.next();
+				
+				VO vo2 = new VO();
+				vo2.setName(name);
+				vo2.setAddress(address);
+				vo2.setPhone(phone);
+				
+				int result = DAO.getInsert(vo2);
+				if(result>0) {
+					list = DAO.getList();
+					test.prn(list);
+				}
+				break;
+			case 4:  
+				System.out.println("삭제할 cust id를 입력하세요>>");
+				String custid2 = scan.next();
+				VO vo3 = new VO();
+				vo3.setCustid(custid2);
+				
+				int result2 = DAO.getDelete(vo3);
+				if(result2>0) {
+					list = DAO.getList();
+					test.prn(list);
+				}
+				
+				break;
+			case 5:  
+				System.out.println("갱신 정보를 입력하세요");
+				System.out.print("갱신할 번호 : ");
+				String custid3 = scan.next();
+				System.out.print("이름 : ");
+				String name2 = scan.next();
+				System.out.print("주소 : ");
+				String address2 = scan.next();
+				System.out.print("전화번호 : ");
+				String phone2 = scan.next();
+				
+				VO vo4 = new VO();
+				vo4.setCustid(custid3);
+				vo4.setName(name2);
+				vo4.setAddress(address2);
+				vo4.setPhone(phone2);
+				
+				int result3 = DAO.getUpdate(vo4);
+				if(result3>0) {
+					list = DAO.getList();
+					test.prn(list);
+				}
+				
+				break;
 
 		default: System.out.println("제대로 입력하세요");break;
 		}
+	}
+	
+	// 테이블 전체보기 메서드
+	public void prn(List<VO> list) {
+		System.out.println("번호\t이름\t주소\t전화번호");
+		for (VO k : list) {
+			System.out.print(k.getCustid()+"\t");
+			System.out.print(k.getName()+"\t");
+			System.out.print(k.getAddress()+"\t");
+			System.out.print(k.getPhone()+"\n");
+		}
+	}
+	
+	public void prn2(VO vo) {
+		System.out.println("번호\t이름\t주소\t전화번호");
+
+		System.out.print(vo.getCustid() + "\t");
+		System.out.print(vo.getName() + "\t");
+		System.out.print(vo.getAddress() + "\t");
+		System.out.print(vo.getPhone() + "\n");
+
 	}
 }
